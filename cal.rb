d4260b9fa8398b6ad77cb614b32e44740e89404c
norisuke <noritomo0424@yahoo.co.jp>
2025-09-18 16:29:31 +0900

    ひな形を作成

--- cal.rb
+++ cal.rb
@@ -1,2 +1,19 @@
 #! /usr/bin/env ruby
-puts "cal"
+
+require 'optparse'
+require 'date'
+
+# main_program
+opt = OptionParser.new
+Date.today
+
+opt.on('-y [VAL]') {|v| year = v }
+opt.on('-m [VAL]') {|v| month = v }
+opt.parse!(ARGV)
+
+month = today.month
+year = today.year
+
+puts "month月　yaer年"
+puts "日　月　火　水　木　金　土"
+puts "日にち"
