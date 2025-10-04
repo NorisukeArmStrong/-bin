af766b787f40527b8a6592dfc1f9b1824f073c4c
norisuke <noritomo0424@yahoo.co.jp>
2025-10-04 15:19:18 +0900

    引数をフレームごとに配列として格納

--- bowling.rb
+++ bowling.rb
@@ -1,10 +1,21 @@
 #! /usr/bin/env ruby
 # frozen_string_literal: true
 
-require 'debug'
-
 score = ARGV[0].split(',')
 shots = []
+i = 0
+frames = []
 score.each do |s|
   shots << (s == 'X' ? 10 : s.to_i)
 end
+
+while i < shots.size && frames.size < 9
+  if shots[i] == 10
+    frames << [shots[i]]
+    i += 1
+  else
+    frames << shots[i, 2]
+    i += 2
+  end
+end
+frames << shots[i..]
