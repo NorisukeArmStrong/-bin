a279376a517d4ed6025be0b7ac63efe27f94bcd5
norisuke <noritomo0424@yahoo.co.jp>
2025-10-04 16:28:54 +0900

    点数計算を記載

--- bowling.rb
+++ bowling.rb
@@ -5,6 +5,7 @@ score = ARGV[0].split(',')
 shots = []
 i = 0
 frames = []
+total = 0
 score.each do |s|
   shots << (s == 'X' ? 10 : s.to_i)
 end
@@ -19,3 +20,22 @@ while i < shots.size && frames.size < 9
   end
 end
 frames << shots[i..]
+
+frames.each_with_index do |f, i|
+  total += if f[0] == 10 && i < 9
+             if frames[i + 1].size < 2
+               f[0] + frames[i + 1][0] + frames[i + 2][0]
+             elsif frames[i + 1].size == 3
+               f[0] + frames[i + 1][0] + frames[i + 1][1]
+             else
+               f[0] + frames[i + 1].sum
+             end
+           elsif i == 9
+             f.sum
+           elsif f.sum == 10
+             f.sum + frames[i + 1][0]
+           else
+             f.sum
+           end
+end
+p total
